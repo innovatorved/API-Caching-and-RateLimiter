2b4bae9d6aa25a341a075ba75c78e5cd081583f1
Ved Gupta <vedgupta@protonmail.com>
2022-04-24 11:41:26 +0530

    basic boilar plate temlate ready need to be attact rate limiter

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const http = require('http');
-const port = process.env.PUBLIC_PORT;
+const express = require("express");
+const bcrypt = require('bcrypt');
 
-const server = http.createServer((req, res) => {
+const path = require('path');
+const app = express();
+
+const port = process.env.PUBLIC_PORT || 3003;
+const host = "http://localhost";
+
+app.get('/' , (req , res)=>{
 	res.statusCode = 200
-	res.setHeader('Content-Type', 'text/plain')
-	res.end('Hello from Next Innovate!')
+	res.setHeader('Content-Type', 'text/html')
+	res.sendFile(path.join(__dirname+'/index.html'))
+});
+
+app.get('/hash/:text/:saltRounds' , (req , res)=>{
+    const plainText = req.params.text;
+    var hashCodedText = 1;
+    try {
+        const saltRounds = parseInt(req.params.saltRounds);
+        hashCodedText = bcrypt.hashSync(plainText, saltRounds);
+    } catch (error) {
+        const saltRounds = 1;
+        hashCodedText = bcrypt.hashSync(plainText, saltRounds);
+    }
+
+    res.statusCode = 200;
+    return res.json({success : true , text : hashCodedText});
 })
 
-server.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}/`)
-})
+console.log(`visit : ${host}:${port}/`);
+app.listen(port);
